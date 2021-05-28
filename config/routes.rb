653e93d1636ef79a8a1d2783c0bad80142b91ef3
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-05-28 10:39:48 +0700

    add image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/products', controller: :products, action: :index
+  get '/contact', controller: :contact, action: :index
+  get '/about', controller: :about, action: :index
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
